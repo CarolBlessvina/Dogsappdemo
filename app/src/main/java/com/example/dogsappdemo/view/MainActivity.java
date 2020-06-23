7a50aadb540a1860103f8fbe70aa1a04b4a93341
Carol Blessvina <blessvinacarol@gmail.com>
2020-06-23 17:18:37 +0200

    Launcher icon added and MVVM folder structure created

--- app/src/main/java/com/example/dogsappdemo/view/MainActivity.java
+++ app/src/main/java/com/example/dogsappdemo/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.dogsappdemo;
+package com.example.dogsappdemo.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.dogsappdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
