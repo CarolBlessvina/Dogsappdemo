63542d78dc7e8b6eac4ceb45f12654f9a583fdb7
megha <kbmegha4@outlook.com>
2020-07-06 14:49:47 +0200

    load the data from API using retrofit

--- app/src/main/java/com/example/dogsappdemo/view/DetailFragment.java
+++ app/src/main/java/com/example/dogsappdemo/view/DetailFragment.java
@@ -2,17 +2,39 @@ package com.example.dogsappdemo.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.telecom.Call;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.dogsappdemo.R;
+import com.example.dogsappdemo.viewmodel.DetailViewModel;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class DetailFragment extends Fragment {
+    private int doguuid;
+    private DetailViewModel viewModel;
 
+@BindView(R.id.dogImage)
+    ImageView dogImage;
+
+@BindView(R.id.dogName)
+    TextView dogName;
+    @BindView(R.id.dogPurpose)
+    TextView dogPurpose;
+    @BindView(R.id.dogTemperament)
+    TextView dogTemperament;
+    @BindView(R.id.lifeSpan)
+    TextView lifeSpan;
     public DetailFragment() {
 
     }
@@ -22,6 +44,16 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+       View view =  inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this,view);
+        return view;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
