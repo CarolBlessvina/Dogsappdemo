05ae793977be059c55255aa3b1cf74985431e8be
megha <kbmegha4@outlook.com>
2020-07-06 22:10:46 +0200

    pull to refresh added

--- app/src/main/java/com/example/dogsappdemo/view/ListFragment.java
+++ app/src/main/java/com/example/dogsappdemo/view/ListFragment.java
@@ -72,6 +72,13 @@ public class ListFragment extends Fragment {
 
         dogsList.setLayoutManager( new LinearLayoutManager(getContext())); // list in linear fashion
         dogsList.setAdapter(dogsListAdapter);
+        refreshLayout.setOnRefreshListener(() -> {
+            dogsList.setVisibility(View.GONE);
+            listError.setVisibility(View.GONE);
+            loadingView.setVisibility(View.VISIBLE);
+            viewModel.refreshBypassCache();
+            refreshLayout.setRefreshing(false);
+        });
         observeViewModel();
     }
     private void observeViewModel() {
