4bca77b3fe89bbbeb0c1bbc17178459da8bd662e
Carol Blessvina <blessvinacarol@gmail.com>
2020-07-07 00:03:18 +0200

    On click detail screen implemented

--- app/src/main/java/com/example/dogsappdemo/viewmodel/DetailViewModel.java
+++ app/src/main/java/com/example/dogsappdemo/viewmodel/DetailViewModel.java
@@ -10,7 +10,7 @@ public class DetailViewModel extends ViewModel {
     public MutableLiveData<DogBreed> dogLiveData = new MutableLiveData<DogBreed>();
     public void fetch()
     {
-        DogBreed dog1 = new DogBreed("1", "corgi", "15 years","","","","" );
+        DogBreed dog1 = new DogBreed("1", "corgi", "15 years","","companionship","calm and friendly","" );
         dogLiveData.setValue(dog1);
 
     }
